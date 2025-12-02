b6724d84f99240ee2efffb5f35af5a41df920c2b
Tatoune <github@gd.pouti.net>
2025-12-02 18:09:37 +0100

    Update BasicsElements.py

--- elements/BasicsElements.py
+++ elements/BasicsElements.py
@@ -3,9 +3,9 @@ Label = QLabel("Text")
 layout.addWidget(Label) # you must use layout.addWidget(Thing) in order for it to be on the window
 
 # Create a QLineEdit
-city_input = QLineEdit()
-city_input.setPlaceholderText("Enter city name...") # Hint text for the user
-layout.addWidget(city_input)
+input = QLineEdit()
+input.setPlaceholderText("Enter city name...") # Hint text for the user
+layout.addWidget(input)
 
 # Create a QPushButton with text
 button = QPushButton("Text")
