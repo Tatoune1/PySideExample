fbff84997bb55226006bba4b3f6174119a3512e5
Tatoune <github@gd.pouti.net>
2025-12-07 17:24:27 +0100

    Update widgets.py
    Addition of the slider widget

--- Templates/widgets.py
+++ Templates/widgets.py
@@ -1,12 +1,15 @@
 # create a QLabel
 Label = QLabel("Text")
-layout.addWidget(Label) # you must use layout.addWidget(Thing) in order for it to be on the window
 
 # Create a QLineEdit
 input = QLineEdit()
 input.setPlaceholderText("text") # Hint text for the user
-layout.addWidget(input)
 
 # Create a QPushButton with text
 button = QPushButton("Text")
-layout.addWidget(button)
+
+# Create a QSlider
+slider = QSlider(tickIntervall = 0, tickPosition = 0) 
+# tickIntervall : spacing between tick marks, measured in slider value units, 0 and the slider will choose between singleStep and pageStep
+# tickPosition : holds the tickmark position for this slider, the default value is 0
+slider.setValue(10) #set the value of the slider to an integer between 0 and 100
