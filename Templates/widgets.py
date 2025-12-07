66043862d73425469639c3b6f107fdca9be7b2eb
Tatoune <github@gd.pouti.net>
2025-12-07 17:26:39 +0100

    Update widgets.py

--- Templates/widgets.py
+++ Templates/widgets.py
@@ -12,4 +12,6 @@ button = QPushButton("Text")
 slider = QSlider(tickIntervall = 0, tickPosition = 0) 
 # tickIntervall : spacing between tick marks, measured in slider value units, 0 and the slider will choose between singleStep and pageStep
 # tickPosition : holds the tickmark position for this slider, the default value is 0
+slider.setMinimum(1)
+slider.setMaximum(50)
 slider.setValue(10) #set the value of the slider to an integer between 0 and 100
